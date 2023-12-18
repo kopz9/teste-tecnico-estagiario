f67b0f835c736cdffcc1a954a7cb098b9a8284e6
kopz <gustavoeduardo438@gmail.com>
2023-12-17 23:50:42 -0300

    feat: sum pairs

--- src/app/questoes/3/page.jsx
+++ src/app/questoes/3/page.jsx
@@ -1,6 +1,19 @@
 const numerosInteiros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+const somaPares = (n) => {
+  let soma = 0;
+
+  for(let i = 0; i < n.length; i++){
+    if(n[i] % 2 === 0){
+      soma+= n[i];
+    } 
+  }
+
+  return soma
+}
+
 export default function Questao3() {
+  const soma = somaPares(numerosInteiros);
   return (
     <>
       <h1>Questão 3</h1>
@@ -8,7 +21,7 @@ export default function Questao3() {
         Escreva uma função JavaScript que recebe uma matriz de números inteiros
         e retorna a soma de todos os números pares. Exiba a soma na tela.
       </p>
-      <p>Soma:</p>
+      <p>Soma: {soma}</p>
     </>
   );
 }
