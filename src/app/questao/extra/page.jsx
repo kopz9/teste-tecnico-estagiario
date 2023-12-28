5ae560c8d66a9080c6dee9b4173078a64133e7ee
kopz9 <gustavoeduardo438@gmail.com>
2023-12-28 17:35:53 -0300

    feat: todo list

--- src/app/questao/extra/page.jsx
+++ src/app/questao/extra/page.jsx
@@ -1,4 +1,35 @@
+'use client';
+import React, {useState, useEffect} from "react";
+import "@/app/components/page.css"
+
+
 export default function QuestaoExtra() {
+const saveLocal = JSON.parse(localStorage.getItem('tasks')) || []
+
+const [tasks, setTasks] = useState(saveLocal)
+const [newTask, setNewTask] = useState('')
+
+
+
+useEffect(() => {
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+})
+
+
+const addNewTask = () => {
+  setTasks([...tasks, {id: Math.random()*1000, title: newTask}])
+  setNewTask('');
+}
+
+const deleteAll = () => {
+  setTasks([])
+}
+
+const delTask = (id) => {
+  setTasks(tasks.filter(tasks => tasks.id !== id))
+  alert(`A tarefa ${tasks.find(tasks => tasks.id === id).title} foi excluida`)
+}
+
   return (
     <>
       <h1>Questão Extra</h1>
@@ -10,6 +41,28 @@ export default function QuestaoExtra() {
         Todos os botões precisam ser redondos e ficarem quadrados quando o mouse estiver sobre eles.
         A lista deve ser salva no localStorage.
       </p>
+      <div className="inputDiv">
+      <input 
+      type='text'
+      value={newTask}
+      onChange={event => setNewTask(event.target.value)}
+      />
+      <button className="addBtn" onClick={addNewTask}>Add</button>
+      <button className="delBtn" onClick={deleteAll}>Deletar todos</button>
+      </div>
+      <ul>
+        {tasks.map(tasks => (
+          <li key={tasks.id}>{tasks.title}<button onClick={() => delTask(tasks.id)}>Delete</button></li>
+        ))}
+
+      </ul>
+
+
     </>
   );
 }
+// lista botao de adicionar botao remover
+// input 
+// cada item da lista deve ter um botao para remover
+// botao para zerar a lista
+// deve ser salva no localstorage
